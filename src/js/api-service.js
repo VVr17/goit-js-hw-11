2f2be7118d0f26adbee16efacce7040497ecb2f0
Vira Voronova <vera.voronova@hotmail.com>
2022-08-31 12:55:49 +0200

    api fixed

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -24,8 +24,6 @@ export default class PicturesApiService {
 
     try {
       const response = await axios.get(this.#BASE_URL, { params });
-      // const response = await axios.get(`https://pixabay.com/api/?key=24377768-1651c24dae1d00899e27f41ae&q=car&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`);
-      // const response = await axios.get(`https://pixabay.com/api/?key=28871664-21007d01445281d8ccfafe378&q=car&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`);
       return response.data;
 
     } catch (error) {
